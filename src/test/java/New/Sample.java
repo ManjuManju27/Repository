ba8ef1e944e8120fe8d025a32469a0bf2a844df2
HP <HP@DESKTOP-QA7IFKP>
2022-08-24 16:10:16 +0530

    updated

--- src/test/java/New/Sample.java
+++ src/test/java/New/Sample.java
@@ -4,7 +4,8 @@ public class Sample {
 
 	public static void main(String[] args) {
 		System.out.println("manju");
-
+System.out.println("Hi manjya.......");
+System.out.println("Hello guys");
 	}
 
 }
